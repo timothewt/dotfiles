ac754f7036b311d5f5f3607be599816e4f4e5f8d
timothewt <22433109+timothewt@users.noreply.github.com>
2024-12-09 00:05:49 +0100

    hyprland

--- .config/nvim/lua/mappings.lua
+++ .config/nvim/lua/mappings.lua
@@ -13,7 +13,8 @@ map("t", "<M-j>", "<Down>")
 map("t", "<M-k>", "<Up>")
 map("t", "<M-h>", "<Left>")
 map("t", "<M-l>", "<Right>")
-
+map({ "n", "v" }, "x", '"_x')
+map({ "n", "v" }, "d", '"_d')
 del("i", "<Tab>")
 vim.g.copilot_no_tab_map = true
 map("i", "<M-y>", "copilot#Accept()", { noremap = true, silent = true, expr = true, replace_keycodes = false }) -- Copilot Accept
